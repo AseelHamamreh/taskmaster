b6ab609fb0936f0148505438e968f3b7892f1d0e
Aseel Hamamreh <aseel.hamamreh@hotmail.com>
2021-08-12 14:37:36 +0300

    lab29 done

--- TaskMaster/app/src/main/java/com/example/taskmaster/AddTaskPage.java
+++ TaskMaster/app/src/main/java/com/example/taskmaster/AddTaskPage.java
@@ -1,24 +1,54 @@
 package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddTaskPage extends AppCompatActivity {
+    private TaskDao taskDao;
+    private AppDataBase appDataBase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Add Task");
         setContentView(R.layout.activity_add_task_page);
-    }
 
-    public void showAlert(View view){
-        Toast.makeText(this,"Submitted!", Toast.LENGTH_LONG).show();
+        //dataBase:
+        appDataBase = Room.databaseBuilder(getApplicationContext(), AppDataBase.class, "tasks").allowMainThreadQueries().build();
+        taskDao = appDataBase.taskDao();
+
+
+        Button addButton = findViewById(R.id.addTaskButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText titleInput = findViewById(R.id.myTitle);
+                TextView bodyInput =  findViewById(R.id.myDescription);
+                TextView statusInput = findViewById(R.id.myStatus);
+                String title = titleInput.getText().toString();
+                String body = bodyInput.getText().toString();
+                String status = statusInput.getText().toString();
+
+                Task task = new Task(title, body, status);
+                taskDao.insertOne(task);
+
+
+                Toast.makeText(AddTaskPage.this,"Task added!", Toast.LENGTH_LONG).show();
+
+
+
+            }
+        });
     }
 
 }
---
+++ TaskMaster/app/src/main/java/com/example/taskmaster/TaskDao.java
@@ -0,0 +1,31 @@
+package com.example.taskmaster;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+
+    @Insert
+    void insertOne(Task task);
+
+    @Query("SELECT * FROM task WHERE task_title LIKE :name")
+    Task findByName(String name);
+
+    @Query("SELECT * FROM task")
+    List<Task> findAll();
+
+    @Delete
+    void deleteItem(Task task);
+}
