ece59f2abd8f2f7fe6259b5372a6f62f250efca5
Aseel Hamamreh <aseel.hamamreh@hotmail.com>
2021-08-15 23:42:36 +0300

    lab31

---
+++ TaskMaster/app/src/androidTest/java/com/example/taskmaster/EspressoTest.java
@@ -0,0 +1,68 @@
+package com.example.taskmaster;
+
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+
+    @Test
+    public void addNewTaskTest(){
+        onView(withId(R.id.AddNewTask)).perform(click());
+
+        onView(withId(R.id.myTitle)).perform(typeText("New Task"),closeSoftKeyboard());
+        onView(withId(R.id.myDescription)).perform(typeText("Do Something"),closeSoftKeyboard());
+        onView(withId(R.id.myStatus)).perform(typeText("in progress"),closeSoftKeyboard());
+
+        onView(withId(R.id.addTaskButton)).perform(click());
+        pressBack();
+
+        onView(ViewMatchers.withId(R.id.songList)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void changingUserNameTest(){
+        onView(withId(R.id.settingsIcon)).perform(click());
+
+        onView(withId(R.id.userName)).perform(typeText("aseel"),closeSoftKeyboard());
+        onView(withId(R.id.addingUserName)).perform(click());
+
+        onView(withId(R.id.textView11)).check(matches(withText("aseel's tasks")));
+    }
+
+    @Test
+    public void detailsPageTest(){
+        onView(ViewMatchers.withId(R.id.songList)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.songList))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+
+        onView(withId(R.id.textTitle)).check(matches(withText("New Task")));
+        onView(withId(R.id.textDes)).check(matches(withText("Do Something")));
+        onView(withId(R.id.textStatus)).check(matches(withText("in progress")));
+    }
+
+
+}
